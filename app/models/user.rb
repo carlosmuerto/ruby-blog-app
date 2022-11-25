e473e351caa37b59e669b83909af9f7e0669ba4d
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-25 12:06:27 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'users_id'
   has_many :comments, foreign_key: 'users_id'
 
+  validates :name, presence: true
+  validates :posts_count, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+   }
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
