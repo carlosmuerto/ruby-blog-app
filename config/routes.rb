e1137042831a540081266e4ba57eca03b54e404b
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-28 14:01:00 -0400

    Add route prefix verb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
+  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get 'users/index'
   get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
