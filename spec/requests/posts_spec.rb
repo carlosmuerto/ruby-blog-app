943a83fd7ca5d36e569524f0e6c038dd167f52d3
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-28 14:13:57 -0400

    Fix basic test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+  describe 'GET /users/:user_id/posts/index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/123/posts/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET /users/:user_id/posts/:id' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/123/posts/321'
       expect(response).to have_http_status(:success)
     end
   end
