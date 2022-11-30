2fd13528a22f32d35f034f533b3bbc1b902aa541
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-30 13:50:37 -0400

    Add Users features specs

---
+++ spec/features/users/user_show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'show one User', type: :feature do
+  let(:user_id) { 1 }
+
+  context "visit /users/:user_id" do
+    before(:example) { visit user_path user_id}
+
+    it "Do not redirect" do
+      expect(current_path).to eq(user_path user_id)
+    end
+
+    it "has placeholder text" do
+      expect(page).to have_text('Users#show')
+    end
+  end
+end
