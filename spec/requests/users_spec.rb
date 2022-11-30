419359eba09aacb81ce66b4d25ce579546259b5b
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-30 13:07:17 -0400

    Add test for users GETs request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  describe ' GET /users' do
+    before(:example) { get users_path }
+
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'users/index' template" do
+      expect(response).to render_template('index')
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET /users/:id' do
+    before(:example) { get user_path 1}
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'user/show' template" do
+      expect(response).to render_template('show')
     end
   end
 end
