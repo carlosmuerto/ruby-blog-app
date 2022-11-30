fb5d6b88acf4eea1bf23fea1aa2a2732bbaeede9
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-30 18:31:50 -0400

    Add users#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  # get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  # get 'users', to: 'users#index', as: 'user_index'
-  # get 'users/:id', to: 'users#show', as: 'user_show'
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
