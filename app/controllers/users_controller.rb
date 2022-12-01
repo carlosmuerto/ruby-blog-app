19ff4b9a0a171bff086707535c8dfb7a8ca3f990
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-30 23:01:54 -0400

    Add User view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def index
-		@users = User.all
+    @users = User.all
   end
 
-  def show; end
+  def show
+    # raise ActionController::RoutingError.new('Not Found')
+    begin
+      @user = User.find params[:id]
+    rescue ActiveRecord::RecordNotFound => e
+			ActiveRecord::RecordNotFound
+    end
+  end
 end
