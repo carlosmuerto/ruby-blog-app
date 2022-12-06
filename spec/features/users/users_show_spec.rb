b9a53597a571c83d9d29db94e067ac0e27e1745d
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-06 11:04:11 -0400

    Update users_show_spec.rb

--- spec/features/users/users_show_spec.rb
+++ spec/features/users/users_show_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'show one User', type: :feature do
   let!(:post_user) do
-    User.first
+    Post.first.author
   end
 
   let!(:posts) do
-    post_user.posts
+    post_user.recent_posts
   end
 
   context 'visit /users/:user_id' do
@@ -24,23 +24,37 @@ describe 'show one User', type: :feature do
       expect(page).to have_text(post_user.name)
     end
 
-    it 'I can see the number of posts the user has written.'
+    it 'I can see the number of posts the user has written.' do
+			expect(page).to have_text("posts: #{posts.count}")
+		end
 
     it "I can see the user's bio." do
       expect(page).to have_text(post_user.bio)
     end
 
     it "I can see the user's first 3 posts." do
-      post_user.recent_posts.each do |post|
-        expect(page).to have_text(post.title)
-        expect(page).to have_text(post.text)
+      posts.each do |post|
+        expect(page).to have_text(post.title.strip)
+        expect(page).to have_text(post.text[0..10].strip)
       end
     end
 
-    it "I can see a button that lets me view all of a user's posts."
+    it "I can see a button that lets me view all of a user's posts." do
+			expect(page).to have_text('see all posts')
+		end
 
-    it "When I click a user's post, it redirects me to that post's show page."
+    it "When I click a user's post, it redirects me to that post's show page." do
+      posts.each do |post|
+				click_link post.title.strip
+        expect(current_path).to eq user_post_path post.author, post
+        visit user_path post_user
+      end
+		end
 
-    it "When I click to see all posts, it redirects me to the user's post's index page."
+    it "When I click to see all posts, it redirects me to the user's post's index page." do
+			click_link 'see all posts'
+			expect(current_path).to eq user_posts_path post_user
+		end
+
   end
 end
