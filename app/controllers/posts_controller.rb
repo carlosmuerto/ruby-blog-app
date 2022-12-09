82a99df0ce0384b3236a815a13709e1878c0ebe3
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-08 22:13:17 -0400

    Add delete comment btn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,30 +9,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.includes(:comments, :author).find params[:id]
-    @comments = @post.comments.accessible_by(current_ability).page(params[:page]).includes(:author)
+    @comments = @post.comments.accessible_by(current_ability).page(params[:page]).includes(:author, post: [:author])
   end
 
   def new
     @post = Post.new
     @post.author = current_user
 
-
-    like = Like.new(author: current_user, post: @post)
-
 		authorize! :create, @post
-  end
-
-  def like
-
-    @post = Post.find params[:post_id]
-
-
-    like = Like.new(author: current_user, post: @post)
-
-		authorize! :create, like
-
-		like.save
-    redirect_to user_post_path @post.author, @post
   end
 
   def create
